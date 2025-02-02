129cc4888af8029080cbda246fd8b2e3d8098883
UwayoOlga <uwayoolga@gmail.com>
2025-02-02 19:29:41 +0200

    creating a frame

---
+++ SWINGAPPS/SimpleAdditionForm.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class SimpleAdditionForm {
+    public static void main(String[] args) {
+     // Create a window using JFrame
+     JFrame frame = new JFrame("Simple Addition Form"); 
+     frame.setSize(400, 400);  
+     frame.setResizable(false);
+     frame.setVisible(true); // making the frame visible
+    }
+}
--- SWINGAPPS/SimpleGUIAdditionApp.java
+++ SWINGAPPS/SimpleGUIAdditionApp.java
@@ -1,14 +1,15 @@
-
-import java.util.Scanner;
-
+// exercise 1 ; adding two nubes 
 import javax.swing.JOptionPane;
 
 public class SimpleGUIAdditionApp {
     public static void main(String[] args){
         int num1, num2, sum;
-        String number1, number2; 
-       // JOptionPane.showMessageDialog(null, " Enter the first number");
+        String number1, number2;  
         number1 = JOptionPane.showInputDialog("Enter the first number");
         number2 = JOptionPane.showInputDialog("Enter the Second number");
+        num1 = Integer.parseInt(number1);
+        num2 = Integer.parseInt(number2);
+        sum = num1 + num2;
+        JOptionPane.showMessageDialog(null, "The sum is " + sum, "the result", JOptionPane.PLAIN_MESSAGE);
     }
 }
