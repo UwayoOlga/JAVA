587f90fefb34d91cdf8966982816e73bd2d72116
UwayoOlga <uwayoolga@gmail.com>
2025-02-02 18:54:18 +0200

    introduction to the GUI using SWING

---
+++ SWINGAPPS/SimpleGUIAdditionApp.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class SimpleGUIAdditionApp {
+    public static void main(String[] args){
+        int num1, num2, sum;
+        String number1, number2; 
+       // JOptionPane.showMessageDialog(null, " Enter the first number");
+        number1 = JOptionPane.showInputDialog("Enter the first number");
+        number2 = JOptionPane.showInputDialog("Enter the Second number");
+    }
+}
