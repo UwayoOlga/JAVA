a0e3756fdcb0a6b3204c9f619ce2e2fe10be4e21
UwayoOlga <uwayoolga@gmail.com>
2025-02-02 20:39:48 +0200

    adding event listener

--- SWINGAPPS/SimpleAdditionForm.java
+++ SWINGAPPS/SimpleAdditionForm.java
@@ -2,14 +2,17 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class SimpleAdditionForm {
     public static void main(String[] args) {
+
      // Create a window using JFrame
      JFrame frame = new JFrame("Simple Addition Form"); 
      frame.setSize(400, 400);  
-     frame.setResizable(false);
+     frame.setResizable(true);
      frame.setVisible(true); // making the frame visible
+
      // Creating Labels, textfields, buttons...(components instances)
      JLabel Num1 = new JLabel("First Number");
      JLabel Num2 = new JLabel("Second Number");
@@ -25,6 +28,7 @@ public class SimpleAdditionForm {
      Sum.setBounds(100, 150, 150, 30);
      ResultDisplay.setBounds(270, 150, 150, 30);
      btnAdd.setBounds(100, 200, 150, 30);
+
       // Adding components to the frame
      frame.setLayout(null);
      frame.add(Num1);
@@ -34,7 +38,15 @@ public class SimpleAdditionForm {
      frame.add(Sum);
      frame.add(ResultDisplay);
      frame.add(btnAdd);
-    
 
+     // Adding an event listener to handle button click
+     btnAdd.addActionListener(new ActionListener() { // A new instance of an ActionListener 
+        public void actionPerformed(ActionEvent e) {  // this method is overridden from the actionlistener
+            int num1 = Integer.parseInt(txtNum1.getText());
+            int num2 = Integer.parseInt(txtNum2.getText());
+            int Sum = num1 + num2;
+            ResultDisplay.setText(Sum+"");
+    }
+});
     }
 }
